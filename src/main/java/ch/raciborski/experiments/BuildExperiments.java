c6df89893de124fea7f072e754cc3aa9308d0cc2
c0desurfer <11145838+c0desurfer@users.noreply.github.com>
2022-07-11 23:12:47 +0200

    experimental commit

---
+++ src/main/java/ch/raciborski/experiments/BuildExperiments.java
@@ -0,0 +1,10 @@
+package ch.raciborski.experiments;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BuildExperiments {
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+}
